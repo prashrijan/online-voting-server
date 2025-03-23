6aec79204980399b4932e272204276227860f877
Prashrijan <shresthaprashrijan@gmail.com>
2025-03-23 16:40:47 +1100

    inital setup for registering user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,1 +1,8 @@
-export const registerUser = async (req, res, next) => {};
+// register user controller
+export const registerUser = async (req, res, next) => {
+    try {
+        // try registering the user
+    } catch (error) {
+        // throw error
+    }
+};
