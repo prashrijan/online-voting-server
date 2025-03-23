fced4501de243da2fef07d263dd2335b5ed1e683
Prashrijan <shresthaprashrijan@gmail.com>
2025-03-23 16:34:26 +1100

    backend setup complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./src/routes/auth.routes.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +11,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("An Online Voting System");
 });
+
+// auth routes
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is ready on http://localhost:${PORT}`);
