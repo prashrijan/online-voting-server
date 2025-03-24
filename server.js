bba7cb1d1b306f7cc8622f58ced1f4a13a3c9b87
Prashrijan <shresthaprashrijan@gmail.com>
2025-03-24 11:36:35 +1100

    added limit to each request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./src/routes/auth.routes.js";
 import { dbConnection } from "./src/db/dbConfig.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,11 +10,21 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+// database connection
 dbConnection()
     .then(() => console.log("Database connected to the server"))
     .catch((error) =>
         console.log(`Database server connection failed: ${error}`)
     );
+
+// rate limit
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    limit: 5,
+    message: "Too many requests from this IP. Please try again later",
+});
+
+app.use(limiter);
 
 app.get("/", (req, res) => {
     res.send("An Online Voting System");
