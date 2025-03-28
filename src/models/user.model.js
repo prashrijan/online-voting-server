9f9f6221a546118babc533a847e77209820b10cf
Prashrijan <shresthaprashrijan@gmail.com>
2025-03-28 14:15:13 +1100

    update this.password to password in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { conf } from "../conf/conf.js";
 
 // user schema
 const userScehma = new Schema(
@@ -44,8 +46,8 @@ const userScehma = new Schema(
 
 // hash password before saving it to the database
 userScehma.pre("save", function (next) {
-    if (!this.isModified(this.password)) return next();
-    this.password = bcrypt.hash(this.password, 10);
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10, (err) => console.log(err));
     next();
 });
 
@@ -55,6 +57,30 @@ userScehma.methods.isPasswordCorrect = function (password) {
 };
 
 // generate access token
+userScehma.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+        },
+        conf.jwtSecret,
+        {
+            expiresIn: conf.jwtExpiry,
+        }
+    );
+};
 // generate refresh token
+userScehma.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+        },
+        conf.refreshSecret,
+        {
+            expiresIn: conf.refreshExpiry,
+        }
+    );
+};
 
 export const User = new mongoose.model("User", userScehma);
