ac03179a5cd077a2d202380759abd0804620013a
Prashrijan <shresthaprashrijan@gmail.com>
2025-03-28 14:22:37 +1100

    login user controller completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,15 +45,15 @@ const userScehma = new Schema(
 );
 
 // hash password before saving it to the database
-userScehma.pre("save", function (next) {
+userScehma.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10, (err) => console.log(err));
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
 // check if password is correct
-userScehma.methods.isPasswordCorrect = function (password) {
-    return bcrypt.compare(password, this.password);
+userScehma.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
 };
 
 // generate access token
