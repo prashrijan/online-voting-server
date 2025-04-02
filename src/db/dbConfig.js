a32bba30b3f57300a0e954f62836872282c2817b
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-04-02 21:18:58 +1100

    commit before merge

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import { conf } from "../conf/conf.js";
+import mongoose from "mongoose"
+import { conf } from "../conf/conf.js"
 
 export const dbConnection = async () => {
-    try {
-        const res = await mongoose.connect(`${conf.mongoDbUrl}/${conf.dbName}`);
-        res && console.log(`Database connection successful.`);
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-};
+  try {
+    const res = await mongoose.connect(`${conf.mongoDbUrl}/${conf.dbName}`)
+    res && console.log(`Database connection successful.`)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
