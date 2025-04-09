a34e195718ea5829a4b7c488ec86631ef526d62c
Prashrijan <shresthaprashrijan@gmail.com>
2025-04-09 18:43:57 +1000

    getting an election through i

--- src/routes/election.route.js
+++ src/routes/election.route.js
@@ -3,6 +3,7 @@ import { authenticateuser, isAdmin } from "../middlewares/authenticateUser.js";
 import {
     createElection,
     getElections,
+    getElection,
 } from "../controllers/election.controller.js";
 import { electionValidatior } from "../middlewares/validation/election/election.validate.js";
 
@@ -10,6 +11,8 @@ const router = express.Router();
 
 // public routes
 router.route("/").get(getElections);
+
+router.route("/:id").get(getElection);
 
 // private route (only admin)
 // create election
