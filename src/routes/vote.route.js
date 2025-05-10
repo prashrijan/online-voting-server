9751baf1d484334bf73752ea6a0a4b7762aea305
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-05-10 14:20:53 +1000

    created cast vote status api

--- src/routes/vote.route.js
+++ src/routes/vote.route.js
@@ -1,14 +1,13 @@
-import express from "express";
-import { authenticateuser, isAdmin } from "../middlewares/authenticateUser.js";
-import {
-    castVote,
-    getElectionResults,
-} from "../controllers/vote.controller.js";
+import express from "express"
+import { authenticateuser, isAdmin } from "../middlewares/authenticateUser.js"
+import { castVote, getElectionResults, checkVoteStatus } from "../controllers/vote.controller.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/").post(authenticateuser, castVote);
+router.route("/").post(authenticateuser, castVote)
 
-router.route("/results/:id").get(authenticateuser, isAdmin, getElectionResults);
+router.route("/results/:id").get(authenticateuser, isAdmin, getElectionResults)
 
-export default router;
+router.route("/checkVoteStatus/:electionId").get(authenticateuser, checkVoteStatus)
+
+export default router
