4ed1a9e4574ace281dbb7a137354a0e24bba591f
Prashrijan <shresthaprashrijan@gmail.com>
2025-05-13 12:51:48 +1000

    fixed date time function

--- src/routes/vote.route.js
+++ src/routes/vote.route.js
@@ -1,20 +1,25 @@
-import express from "express"
-import { authenticateuser, isAdmin } from "../middlewares/authenticateUser.js"
+import express from "express";
+import { authenticateuser, isAdmin } from "../middlewares/authenticateUser.js";
 import {
-  castVote,
-  getElectionResults,
-  checkVoteStatus,
-  getLiveVoteData,
-} from "../controllers/vote.controller.js"
+    castVote,
+    getElectionResults,
+    checkVoteStatus,
+    getLiveVoteData,
+    getVoterCounts,
+} from "../controllers/vote.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/").post(authenticateuser, castVote)
+router.route("/").post(authenticateuser, castVote);
 
-router.route("/results/:id").get(authenticateuser, isAdmin, getElectionResults)
+router.route("/results/:id").get(authenticateuser, isAdmin, getElectionResults);
 
-router.route("/checkVoteStatus/:electionId").get(authenticateuser, checkVoteStatus)
+router
+    .route("/checkVoteStatus/:electionId")
+    .get(authenticateuser, checkVoteStatus);
 
-router.route("/:electionId/live").get(authenticateuser, getLiveVoteData)
+router.route("/:electionId/live").get(authenticateuser, getLiveVoteData);
 
-export default router
+router.route("/get-voters-count/:electionId").get(getVoterCounts);
+
+export default router;
