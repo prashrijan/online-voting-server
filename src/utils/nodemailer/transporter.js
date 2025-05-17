caf77bd960ff2d338c5cff28c55e0e2db7fa6cac
Prashrijan <shresthaprashrijan@gmail.com>
2025-05-17 18:15:43 +1000

    made it so email comes in gmail

--- src/utils/nodemailer/transporter.js
+++ src/utils/nodemailer/transporter.js
@@ -2,8 +2,7 @@ import nodemailer from "nodemailer";
 import { conf } from "../../conf/conf.js";
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
+    service: "gmail",
     auth: {
         user: conf.emailUser,
         pass: conf.emailPass,
